dbfd8e15888dd4f6f4aa229d366b33694e26c5d0
Husan <102375119+Enthusiasticguy@users.noreply.github.com>
2022-04-14 22:10:38 +1200

    covid19graphics.py
    Covid19ni rasmini chizishni o'rganamiz

---
+++ covid19graphics.py
@@ -0,0 +1,23 @@
+# Covid19ni rasmini chizish
+#  turtle modulini import qilib olamiz
+from turtle import *
+# uni 5 tezlikda chizishini aytamiz
+speed(5)
+# orqa fonni qora rangda qilishni aytamiz
+bgcolor("black")
+# yashil rangda chizishini aytamiz
+color("green")
+# i degan o'zgaruvchi yaratamiz
+i=200
+while i>0:
+  # sikl to'xtaguncha chapga yuradi
+  left(i)
+  # sikl to'xtaguncha oldinga yuradi
+  forward(i*3)
+  # i o'zgaruvchini bittaga kamaytiramiz
+  i=i-1
+# va bajaramiz
+  done()  
+  
+  
+  
